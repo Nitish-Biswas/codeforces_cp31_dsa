f754fee2081bee88898862b79ec002d45d2fc87d
Nitish Biswas <nitishbiswas066@gmail.com>
2025-10-31 21:56:06 +0530

    31_oct

---
+++ 1901_A_line_trip.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+
+    cin >> t;
+    while (t--) {
+        int n,x; 
+        cin >> n >>x;
+        int ans =0;
+        int prev = 0;
+
+        int a;
+        for (int i = 0; i < n; i++){
+            cin >> a;
+            ans = max(ans, a-prev);
+            prev = a;
+        }
+
+        ans = max(ans, 2*(x - prev));
+        cout << ans << endl;
+
+        
+    }
+    return 0;
+
+}
